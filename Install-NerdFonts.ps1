1092c07f6af6662039fb93c6798b1dcae182d0c2
Habibi Med Wassim <habibimedwassim@gmail.com>
2025-10-23 13:35:36 +0100

    add synopsis

---
+++ Install-NerdFonts.ps1
@@ -0,0 +1,9 @@
+#!/usr/bin/env pwsh
+<#
+.SYNOPSIS
+    Download and (optionally) install Nerd Fonts on Windows.
+
+.DESCRIPTION
+    Downloads selected Nerd Fonts from GitHub and either saves them locally
+    or installs them system-wide (requires Administrator privileges).
+#>
